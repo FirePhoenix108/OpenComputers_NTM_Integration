18d3835217eee543f49131e21ce46c772c252a99
FirePhoenix <martuccipiersilvio@gmail.com>
2026-02-03 23:35:36 +0100

    Fixed, but still not working.

--- rbmk_controller.lua
+++ rbmk_controller.lua
@@ -1,11 +1,14 @@
 local component = require("component")
 
-local rbmk_ctrl_rod = rbmk_control_rod.component 
+local rbmkCons = component.rbmk_console 
 local rod_level = io.read()
 
 io.write("Type the level: ")
 
 level = tonumber(rod_level)
 
-rbmk_ctrl_rod = setLevel(level)
+if level then
+    rbmkCons.setLevel(level)
+    io.write(level)
+end
 
