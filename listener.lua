968c9d76fc0605e1d025fe5c7bdfd6accbee9863
FirePhoenix <102532785+FirePhoenix108@users.noreply.github.com>
2026-02-04 09:36:00 +0100

    Added a listener script.
    Just for receiving messages. Useful on a PC always on. To be fixed and updated.

---
+++ listener.lua
@@ -0,0 +1,15 @@
+--- This script is created to listen to incoming messages. You can use it
+--- On multiple PC's.
+
+
+local component = require("component")
+
+local net = component.modem
+local input = io.read()
+
+io.write("Please, type the port you want to listen and receive messages: ")
+local port = tonumber(input)
+
+-- Wait for a message from another network card. (Yes, i copied it from the documentation.)
+local _, _, from, port, _, message = event.pull("modem_message")
+print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
