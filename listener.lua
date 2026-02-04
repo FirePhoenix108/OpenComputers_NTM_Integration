708988318256ec464214c489bacc2b6f0c3c1487
FirePhoenix <martuccipiersilvio@gmail.com>
2026-02-04 17:07:25 +0100

    Fixed the listener file with missing libraries, and alghorithm.

--- listener.lua
+++ listener.lua
@@ -3,12 +3,21 @@
 
 
 local component = require("component")
+local event = require("event")
 
 local net = component.modem
 local input = io.read()
 
 io.write("Please, type the port you want to listen and receive messages: ")
 local port = tonumber(input)
+
+if port then
+    net.open(port)
+    io.write("Port opened on" .. port .. "."\n)
+else
+    io.write("Error: incorrect port or port already open.")
+    return
+end
 
 while true do
     -- Wait for a message from another network card. (Yes, i copied it from the documentation.)
