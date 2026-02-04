9b450f3b380c594fdcd365e23e344031216fdfb8
FirePhoenix <martuccipiersilvio@gmail.com>
2026-02-04 16:29:08 +0100

    Added an os sleep option, and a cycle.

--- listener.lua
+++ listener.lua
@@ -7,9 +7,12 @@ local component = require("component")
 local net = component.modem
 local input = io.read()
 
-io.write("Please, type the port you want to listen and receive messages: ")
-local port = tonumber(input)
+while true do
+    io.write("Please, type the port you want to listen and receive messages: ")
+    local port = tonumber(input)
 
--- Wait for a message from another network card. (Yes, i copied it from the documentation.)
-local _, _, from, port, _, message = event.pull("modem_message")
-print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
+    -- Wait for a message from another network card. (Yes, i copied it from the documentation.)
+    local _, _, from, port, _, message = event.pull("modem_message")
+    print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
+    os.sleep(0.5)
+end
