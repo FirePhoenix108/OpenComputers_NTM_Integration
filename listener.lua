64b19aa388e15dc374876c9ff9c77676b7376d36
FirePhoenix <martuccipiersilvio@gmail.com>
2026-02-04 16:57:53 +0100

    Good looking.

--- listener.lua
+++ listener.lua
@@ -11,8 +11,6 @@ io.write("Please, type the port you want to listen and receive messages: ")
 local port = tonumber(input)
 
 while true do
-
-
     -- Wait for a message from another network card. (Yes, i copied it from the documentation.)
     local _, _, from, port, _, message = event.pull("modem_message")
     print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
