a13dd37b2097c8fe4af752887d83e39b551cb009
FirePhoenix <martuccipiersilvio@gmail.com>
2026-02-07 18:08:25 +0100

    Fixed and upgraded. Have fun!

--- member_grade.lua
+++ member_grade.lua
@@ -6,11 +6,13 @@ io.write("WARNING!\n")
 io.write("This is the user database. Please be careful to what you are doing here!\n\n\n")
 
 local usersInfo = "/home/database/users.txt"
+local databaseDir = "/home/database"
 
-if not fs.exists(usersInfo) then
-    io.write("WARNIGN!\nThe directory is missing.")
-    io.write("Creating it for you...\n")
+if not fs.exists(databaseDir) then
+    io.write("WARNIGN!\nThe directory is missing.\n")
+    io.write("Creating it for you...\n\n\n")
     fs.makeDirectory("/home/database")
+    os.sleep(1.0)
     io.write("A directory in /home named database has been created!\n")
     io.write("Please, run again the program to make changes effective.\n")
     return
@@ -41,29 +43,35 @@ local function databaseRead(usersInfo)
     return savedInfo or {}
 end
 
-io.write("Do you want to make a new user or see the already registered ones?\n(Write '1' for new user or '2' to watch the database)...\n")
-local input = io.read()
+os.sleep(1.0)
 
-if input == "1" then
-    io.write("Please write the username: ")
-    local username = io.read()
-    io.write("Please write the password: ")
-    local pssw = io.read()
-    local newUser = { password = pssw }
-    local database = databaseRead(usersInfo)
-    database[username] = newUser
-    databaseSave(usersInfo, database)
-    io.write("User succesfully created!\n")
-    return
-end
+local input = ""
 
-if input == "2" then 
-    io.write("---- REGISTERED USERS LIST ----\n")
-    local database = databaseRead(usersInfo)
-    for username, info in pairs(database) do
-        io.write("User: " .. username .. " | Password: " .. info.password)
+while input ~= "q" do
+    io.write("\n1: New User | 2: View Database | q: Exit\n")
+    io.write("Choice: ")
+    input = io.read()
+    
+    if input == "1" then
+        io.write("Please write the username: ")
+        local username = io.read()
+        io.write("Please write the password: ")
+        local pssw = io.read()
+        local newUser = { password = pssw }
+        local database = databaseRead(usersInfo)
+        database[username] = newUser
+        databaseSave(usersInfo, database)
+        io.write("User succesfully created!\n")
     end
 
-    io.write("---------------------------\n")
+    if input == "2" then 
+        io.write("---- REGISTERED USERS LIST ----\n")
+        local database = databaseRead(usersInfo)
+        for username, info in pairs(database) do
+            io.write("\nUser: " .. username .. " | Password: " .. info.password)
+        end
+
+        io.write("---------------------------\n")
+    end
 end
 
